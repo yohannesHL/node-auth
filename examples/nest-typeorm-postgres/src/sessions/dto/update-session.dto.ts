82dc7e869dc145b6928b3fd445e35fadd26d8cff
Yohannes Libanos <yohannes@fluxsoft.dev>
2020-10-28 19:34:51 +0000

    Remove Sessions Module

--- examples/nest-typeorm-postgres/src/sessions/dto/update-session.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSessionDto } from './create-session.dto';
-
-export class UpdateSessionDto extends PartialType(CreateSessionDto) {}
