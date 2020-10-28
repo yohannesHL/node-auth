7cfcc70fe28463400511a9d3c443de2e3fdf46ba
Yohannes Libanos <yohannes@fluxsoft.dev>
2020-10-28 21:29:20 +0000

    remove unused import

--- examples/nest-typeorm-postgres/src/app.module.ts
+++ examples/nest-typeorm-postgres/src/app.module.ts
@@ -5,7 +5,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { join } from 'path';
 
 @Module({
   imports: [
