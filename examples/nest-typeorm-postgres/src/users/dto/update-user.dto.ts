7973f471f2c33b989f2c8004cb2e54190b15053a
Yohannes Libanos <yohannes@fluxsoft.dev>
2020-10-28 18:59:03 +0000

    Use provided @nestjs/mapped-types

--- examples/nest-typeorm-postgres/src/users/dto/update-user.dto.ts
+++ examples/nest-typeorm-postgres/src/users/dto/update-user.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-    readonly userName: string;
-    readonly firstName?: string;
-    readonly lastName?: string;
-    readonly password?: string;
-    readonly email?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
