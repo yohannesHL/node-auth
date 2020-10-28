bbec62b14d365400e8df7a8d66a208a99f011e34
Yohannes Libanos <yohannes@fluxsoft.dev>
2020-10-28 21:40:40 +0000

    Auto create tables

--- examples/nest-typeorm-postgres/src/app.module.ts
+++ examples/nest-typeorm-postgres/src/app.module.ts
@@ -15,7 +15,8 @@ import { UsersModule } from './users/users.module';
       username: config.TYPEORM_USERNAME,
       password: config.TYPEORM_PASSWORD,
       database: config.TYPEORM_DATABASE,
-      autoLoadEntities: true
+      autoLoadEntities: true,
+      synchronize:true
     }),
     UsersModule,
     AuthModule,
